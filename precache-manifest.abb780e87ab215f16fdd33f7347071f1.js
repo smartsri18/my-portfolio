478632e887105f94f55158193f3f802e9e283967
smartsri18 <sridharan479@gmail.com>
2020-08-14 12:41:52 +0530

    Updates

---
+++ precache-manifest.abb780e87ab215f16fdd33f7347071f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32388ffde0e8ef84fb3264203eb8b7e",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "aa01690b6384236a600a",
+    "url": "/my-portfolio/static/css/2.8c191d56.chunk.css"
+  },
+  {
+    "revision": "f1ec7a29437ff8976ad2",
+    "url": "/my-portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aa01690b6384236a600a",
+    "url": "/my-portfolio/static/js/2.dead89c9.chunk.js"
+  },
+  {
+    "revision": "8cec868430f88511b99b88768b49f102",
+    "url": "/my-portfolio/static/js/2.dead89c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ec7a29437ff8976ad2",
+    "url": "/my-portfolio/static/js/main.150a235a.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
